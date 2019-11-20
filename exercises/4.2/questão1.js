e76cdc33e2a7237dc3ebeeea2f54ca9487b2ee88
laurolyra <laurolyra@gmail.com>
2019-11-20 20:01:14 -0300

    questão de 1 a 4

---
+++ exercises/4.2/questão1.js
@@ -0,0 +1,9 @@
+let n = 4;
+if (n<=1){
+    console.log("número inválido")
+}
+else{
+    for (i=0;i<n;i++){
+        console.log("*".repeat(n));
+    }
+}
---
+++ exercises/4.2/questão3.js
@@ -0,0 +1,9 @@
+let n = 5;
+if (n<=1){
+    console.log("número inválido")
+}
+else{
+    for (i=0;i<n;i++){
+        console.log("*".repeat(i+1));
+    }
+}
