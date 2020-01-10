e2cc94aaa2955d4b11d3b3dbd98abd2df9dd52be
laurolyra <laurolyra@gmail.com>
2020-01-10 17:51:39 -0300

    exercício 3

--- exercises/8.4/exercise3.js
+++ exercises/8.4/exercise3.js
@@ -9,7 +9,7 @@ class Dog {
 let dog = new Dog("Dog!!!")
 
 assert.equal(typeof Dog.prototype, "object")
-assert.ok(dog instanceof Dog) //até aqui, tudo bem
+assert.ok(dog instanceof Dog)
 assert.equal(dog.name, "")
 dog.name = "Peter"
 assert.equal(dog.name, "Peter")
