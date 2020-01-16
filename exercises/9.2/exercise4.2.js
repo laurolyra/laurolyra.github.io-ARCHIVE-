d7bdb9df2fc3c0a0894663a266270a33c9b61835
laurolyra <laurolyra@gmail.com>
2020-01-16 19:30:16 -0300

    resolução dos exercícios do dia 9.2

---
+++ exercises/9.2/exercise4.2.js
@@ -0,0 +1,31 @@
+const messageDelay = () => Math.floor(Math.random() * 5000)
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58
+  return Math.floor(Math.random()  * maxTemperature)
+}
+
+const toFarenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32
+const temperatureInFarenheit = (temperature) => console.log(`It is currently ${toFarenheit(temperature)}ºF at Mars`)
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`)
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`)
+
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (funcaodeCallback) => {
+  const didOperationSucceed = Math.random() >= 0.6
+  if (didOperationSucceed === true)
+  {
+  const tempCelsius = getMarsTemperature();
+  funcaodeCallback(tempCelsius)
+} else {
+  const errorMessage = 'Robot is busy'
+  handleError(errorMessage);
+}
+}
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFarenheit, handleError)
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError)
