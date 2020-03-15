622b2a346729e5ab24b300902160e0792d11adff
lauro <laurolyra@gmail.com>
2020-03-15 19:17:08 -0300

    Updates

---
+++ precache-manifest.2fa41002988f93e09c4f95606697ba01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6199b511860bb4c4443bd8aed7301187",
+    "url": "/lauro-responsive-todolist/index.html"
+  },
+  {
+    "revision": "fb9d012df4b3f88cf1e0",
+    "url": "/lauro-responsive-todolist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f04cc6cb11147a431f52",
+    "url": "/lauro-responsive-todolist/static/js/2.6866a568.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lauro-responsive-todolist/static/js/2.6866a568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9d012df4b3f88cf1e0",
+    "url": "/lauro-responsive-todolist/static/js/main.c96d9316.chunk.js"
+  },
+  {
+    "revision": "910edd0248bb6ed0225f",
+    "url": "/lauro-responsive-todolist/static/js/runtime-main.11b5619a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lauro-responsive-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
